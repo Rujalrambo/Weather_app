4807c47f93aa4122ae470088900724fff2122771
Rujal <rujalshrsth@gmail.com>
2024-07-04 19:16:38 +0545

    Error msg adjusted

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,5 +1,6 @@
 const apiKey = "d781398a86daf705c94504adb20400cb";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
@@ -8,34 +9,42 @@ const weatherIcon = document.querySelector(".weather-icon");
 async function checkWeather(city) {
   try {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    if (!response.ok) {
-      throw new Error("City not found");
+
+    // if (!response.ok) {
+    //   throw new Error("City not found");
+    // }
+
+    if (response.status == 404) {
+      document.querySelector(".error").style.display = "block";
+      document.querySelector(".weather").style.display = "none";
+    } else {
+      const data = await response.json();
+
+      console.log(data);
+      document.querySelector(".city").innerHTML = data.name;
+      document.querySelector(".temp").innerHTML =
+        Math.round(data.main.temp) + "°C";
+      document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+      document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
+
+      if (data.weather[0].main === "Clouds") {
+        weatherIcon.src = "images/clouds.png";
+      } else if (data.weather[0].main === "Rain") {
+        weatherIcon.src = "images/rain.png";
+      } else if (data.weather[0].main === "Clear") {
+        weatherIcon.src = "images/clear.png";
+      } else if (data.weather[0].main === "Drizzle") {
+        weatherIcon.src = "images/drizzle.png";
+      } else if (data.weather[0].main === "Mist") {
+        weatherIcon.src = "images/mist.png";
+      } else if (data.weather[0].main === "Snow") {
+        weatherIcon.src = "images/snow.png";
+      }
+
+      document.querySelector(".weather").style.display = "block";
     }
-    const data = await response.json();
-
-    console.log(data);
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
-
-    if (data.weather[0].main === "Clouds") {
-      weatherIcon.src = "images/clouds.png";
-    } else if (data.weather[0].main === "Rain") {
-      weatherIcon.src = "images/rain.png";
-    } else if (data.weather[0].main === "Clear") {
-      weatherIcon.src = "images/clear.png";
-    } else if (data.weather[0].main === "Drizzle") {
-      weatherIcon.src = "images/drizzle.png";
-    } else if (data.weather[0].main === "Mist") {
-      weatherIcon.src = "images/mist.png";
-    } else if (data.weather[0].main === "Snow") {
-      weatherIcon.src = "images/snow.png";
-    }
-
-    document.querySelector(".weather").style.display = "block";
   } catch (error) {
-    alert(error.message);
+    // alert(error.message);
   }
 }
 
